58465b2d23114c8e2786910f6648482837964816
Florian Braun <Braunf25@proton.me>
2025-10-10 19:04:09 +0200

    Initial Commit

---
+++ Waypoint/WaypointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointApp.swift
+//  Waypoint
+//
+//  Created by Florian Braun on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WaypointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
